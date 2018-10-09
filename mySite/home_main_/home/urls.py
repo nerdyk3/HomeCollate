f09da6218725b5e9098829c76a4bcd2044a37506
coderk3 <kunalkumar.kus@gmail.com>
2018-10-09 18:05:01 +0530

    done

--- mySite/home_main_/home/urls.py
+++ mySite/home_main_/home/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('compare/',views.compare,name='compare'),
     path('result/',views.result,name='result'),
+    path('about-us/',views.about_us,name='about_us'),
+    path('FAQs/',views.faqs,name='faqs'),
+    path('partner-with-us/',views.partner_with_us,name='partner_with_us'),
+    path('privacy/',views.privacy,name='privacy'),
+    path('T&C/',views.T_C,name='T_C'),
+    path('why-us/',views.why_us,name='why_us'),
 ]
--- mySite/home_main_/home/views.py
+++ mySite/home_main_/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse
-from .form import dummyForm
+from .form import dummyForm,dummyForm2
 from CompanyRegistration.models import City ,Company_Registration
 from PropertyRegistration.models import Property_Registration
 import pdb
@@ -20,7 +20,7 @@ def compare(request):
             D = d.save(commit=False)
             Dname = D.name
             city3(Dname)
-            city1 = Company_Registration.objects.filter(city__name=D.name)
+            city1 = Company_Registration.objects.filter(city__name=D.name).order_by('priceMini')
             # for item in Company_Registration.objects.all():
             return render(request,'index.html',{'data':data,'comp_data':city1})
     else:
@@ -34,8 +34,30 @@ def city3(Dname):
     return 0
 
 
-def result(request):
-    city2 =Property_Registration.objects.filter(Area__name=city4)
-    return render(request,'result.html',{'result':city2})
+# def result(request):
+#     city2 =Property_Registration.objects.filter(Area__name=city4)
+#     return render(request,'result.html',{'result':city2})
 
-        
+def about_us(request):
+    return render(request,'about-us.html')
+def faqs(request):
+    return render(request,'FAQs.html')
+def partner_with_us(request):
+    return render(request,'partner-with-us.html')
+def privacy(request):
+    return render(request,'privacy.html')
+def T_C(request):
+    return render(request,'T&C.html')
+def why_us(request):
+    return render(request,'why-us.html')
+
+def result(request):
+    if request.method== "POST":
+        dummycompany = dummyForm2(request.POST)
+        if dummycompany.is_valid():
+            Dummycompany =dummycompany.save(commit=False)
+            DummyCompany = Dummycompany.company
+            Company_PG =Property_Registration.objects.filter(Company__company=DummyCompany)
+            return render(request,'search_result.html',{'Company_PG':Company_PG})
+    else:
+        return HttpResponse("Please search_resul")
